736adae86ca78991f98a87cdca07ddb6bdf2223b
AnnieBoyor <65744814+boyorannie@users.noreply.github.com>
2023-07-20 18:42:42 +0000

    Update division.py

--- division.py
+++ division.py
@@ -1,1 +1,6 @@
+nombre1 = float(input("Entrez le premier nombre : ")
+nombre2= float(input("Entrez le deuxième nombre : "))
 
+resultat = nombre1 / nombre2
+
+print("Le résultat de la division est :", resultat)
